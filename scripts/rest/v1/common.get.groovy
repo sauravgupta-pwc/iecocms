b4e147dd95decf661d61a88914b3ac49ac7685b9
admin admin <evaladmin@example.com>
2019-08-21 06:35:29 +0000

    Delete file /scripts/rest/v1/common.get.groovy

--- scripts/rest/v1/common.get.groovy
+++
@@ -1,6 +0,0 @@
-import org.craftercms.engine.model.SiteItem
-
-SiteItem item = siteItemService.getSiteItem("/site/components/contents${params.site}");
-if(item!=null)
-    return item.getDom();
-return ["err_code":"404","err_message":"Mentioned page not found"];
