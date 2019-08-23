38a84481afa5764051c26c66fc6ff4a0456f4d29
admin admin <evaladmin@example.com>
2019-08-23 12:12:36 +0000

    User admin wrote content /scripts/rest/v1/test.get.groovy

---
+++ scripts/rest/v1/test.get.groovy
@@ -0,0 +1,17 @@
+import org.craftercms.engine.model.SiteItem;
+import org.dom4j.Document;
+
+SiteItem item = siteItemService.getSiteItem("/site/components/contents${params.site}");
+if(item==null)
+    return ["err_code":"404","err_message":"Mentioned page not found"];
+Document document = item.getDom();
+document.selectSingleNode("component/content-type").detach();
+document.selectSingleNode("component/display-template").detach();
+document.selectSingleNode("component/merge-strategy").detach();
+document.selectSingleNode("component/objectGroupId").detach();
+document.selectSingleNode("component/objectId").detach();
+document.selectSingleNode("component/folder-name").detach();
+document.selectSingleNode("component/file-name").detach();
+document.selectSingleNode("component/internal-name").detach();
+
+return document;
