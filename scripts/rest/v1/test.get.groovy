3ca32e08675a974ad49ec4c1a8ae13001bdc43d4
admin admin <evaladmin@example.com>
2019-09-02 10:42:41 +0000

    User admin wrote content /scripts/rest/v1/test.get.groovy

--- scripts/rest/v1/test.get.groovy
+++ scripts/rest/v1/test.get.groovy
@@ -3,10 +3,11 @@ import java.net.URL
 def get = new URL("http://iecocms.eastus.cloudapp.azure.com:8080/studio/api/2/audit").openConnection();
 def getRC = get.getResponseCode();
 println(getRC);
+return getRC;
 if(getRC.equals(200)) {
     //println(get.getInputStream().getText());
     return get.getInputStream().getText();
     //return "Success";
 }
 
-return "Failed";
+//return "Failed";
