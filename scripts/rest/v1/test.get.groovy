bb58f68fb85dc8c40b856c7d96135cdd55d7a42c
admin admin <evaladmin@example.com>
2019-09-02 10:29:41 +0000

    User admin wrote content /scripts/rest/v1/test.get.groovy

--- scripts/rest/v1/test.get.groovy
+++ scripts/rest/v1/test.get.groovy
@@ -1,6 +1,6 @@
 import java.net.URL
 
-def get = new URL("http://iecocms.eastus.cloudapp.azure.com:8080/studio/api/2/audit").openConnection();
+def get = new URL("http:localhost:8080/studio/api/2/audit").openConnection();
 def getRC = get.getResponseCode();
 logger.info('flag 1')
 println(getRC);
