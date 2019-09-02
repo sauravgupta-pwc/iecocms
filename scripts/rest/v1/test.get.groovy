f01c5f7d54cc0e6e9c4780d9b81a91e7b2d9f670
admin admin <evaladmin@example.com>
2019-09-02 10:18:41 +0000

    User admin wrote content /scripts/rest/v1/test.get.groovy

--- scripts/rest/v1/test.get.groovy
+++ scripts/rest/v1/test.get.groovy
@@ -1,18 +1,9 @@
-import org.craftercms.engine.model.SiteItem;
-import org.dom4j.Document;
+import java.net.URL
 
-SiteItem item = siteItemService.getSiteItem("/site/components/contents${params.site}");
-if(item==null)
-    return ["err_code":"404","err_message":"Mentioned page not found"];
-Document document = item.getDom();
-document.selectSingleNode("component/content-type").detach();
-document.selectSingleNode("component/display-template").detach();
-document.selectSingleNode("component/merge-strategy").detach();
-document.selectSingleNode("component/objectGroupId").detach();
-document.selectSingleNode("component/objectId").detach();
-document.selectSingleNode("component/folder-name").detach();
-document.selectSingleNode("component/file-name").detach();
-document.selectSingleNode("component/internal-name").detach();
-
-
-return document.lastModifiedDate;
+URL get = new URL("http://iecocms.eastus.cloudapp.azure.com:8080/studio/api/1/services/api/1/audit/get.json?site_id=ieco_cms&start=0&number=50&actions=["UPDATED"]");
+get.openConnection();
+def getRC = get.getResponseCode();
+println(getRC);
+if(getRC.equals(200)) {
+    println(get.getInputStream().getText());
+}
