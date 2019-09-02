22940293d7d71a994548d370c43acbbeec881fd8
admin admin <evaladmin@example.com>
2019-09-02 10:36:44 +0000

    User admin wrote content /scripts/rest/v1/test.get.groovy

--- scripts/rest/v1/test.get.groovy
+++ scripts/rest/v1/test.get.groovy
@@ -2,11 +2,11 @@ import java.net.URL
 
 def get = new URL("https://docs.oracle.com/javase/8/docs/api/overview-summary.html").openConnection();
 def getRC = get.getResponseCode();
-logger.info('flag 1')
 println(getRC);
 if(getRC.equals(200)) {
-    println(get.getInputStream().getText());
-    return get.getInputStream().getText();
+    //println(get.getInputStream().getText());
+    //return get.getInputStream().getText();
+    return "Success";
 }
 
-logger.info('flag 2')
+return "Failed";
