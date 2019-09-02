958a76639706baab2172418aa39c4fa3a3421a21
admin admin <evaladmin@example.com>
2019-09-02 12:15:22 +0000

    User admin wrote content /scripts/rest/v1/postData.get.groovy

--- scripts/rest/v1/postData.get.groovy
+++ scripts/rest/v1/postData.get.groovy
@@ -13,8 +13,8 @@ def postRC = post.getResponseCode();
 println(postRC);
 if(postRC.equals(200)) {
     //println(post.getInputStream().getText());
-    //return post.getInputStream().getText();
-    return post.getInputStream().cookies();
+    return post.getInputStream().getText();
+    //return post.getInputStream().cookies();
 }
 
 
