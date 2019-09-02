70627949d66637b21f8d44b6887c582a37265941
admin admin <evaladmin@example.com>
2019-09-02 11:32:28 +0000

    User admin wrote content /scripts/rest/v1/postData.get.groovy

--- scripts/rest/v1/postData.get.groovy
+++ scripts/rest/v1/postData.get.groovy
@@ -1,13 +1,19 @@
 import java.net.URL
 
-def get = new URL("http://iecocms.eastus.cloudapp.azure.com:8080/studio/api/2/audit").openConnection();
-def getRC = get.getResponseCode();
-println(getRC);
-return getRC;
-if(getRC.equals(200)) {
-    //println(get.getInputStream().getText());
-    return get.getInputStream().getText();
-    //return "Success";
+def post = new URL("http://iecocms.eastus.cloudapp.azure.com:8080/studio/api/1/services/api/1/security/login.json").openConnection();
+def message = '{ "username" : "admin", "password" : "Q!w2e3r4t5y6" }'
+post.setRequestMethod("POST")
+post.setDoOutput(true)
+post.setRequestProperty("Content-Type", "application/json")
+post.setRequestProperty("Cookie", "XSRF-TOKEN=1234567890")
+post.setRequestProperty("X-XSRF-TOKEN", "1234567890")
+
+post.getOutputStream().write(message.getBytes("UTF-8"));
+def postRC = post.getResponseCode();
+println(postRC);
+if(postRC.equals(200)) {
+    println(post.getInputStream().getText());
+    return "Success";
 }
 
-//return "Failed";
+return "Failed";
