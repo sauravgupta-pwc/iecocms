cae51fe9025be0a32e6e0807e2b5353890b4c045
admin admin <evaladmin@example.com>
2019-10-24 11:28:48 +0000

    User admin wrote content /scripts/rest/v1/common2.get.groovy

--- scripts/rest/v1/common2.get.groovy
+++ scripts/rest/v1/common2.get.groovy
@@ -1,8 +1,1 @@
-/*import org.craftercms.engine.model.SiteItem
-
-SiteItem item = siteItemService.getSiteItem("/site/components/contents${params.site}");
-if(item!=null)
-    return item.getDom();
-return ["err_code":"404","err_message":"Mentioned page not found"];*/
-
 return ${params.site};
